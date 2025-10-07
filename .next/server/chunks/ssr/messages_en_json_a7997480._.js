cc2b19679301af8ef9151f4d82e1f2513d79f1fe
ahmed123humran <ahm88m%0@gmail.com>
2025-10-07 18:44:53 +0300

    second  branch

--- .next/server/chunks/ssr/messages_en_json_a7997480._.js
+++ .next/server/chunks/ssr/messages_en_json_a7997480._.js
@@ -1,5 +1,3 @@
-module.exports = [
-"[project]/messages/en.json (json)", ((__turbopack_context__) => {
+module.exports=[50929,a=>{a.v(JSON.parse('{"MenuItem":{"home":"Home","services":"Services we offer"},"Header":{"contactus":"Contact Us"}}'))}];
 
-__turbopack_context__.v(JSON.parse("{\"MenuItem\":{\"home\":\"Home\",\"services\":\"Services we offer\"},\"Header\":{\"contactus\":\"Contact Us\"}}"));}),
-];
+//# sourceMappingURL=messages_en_json_a7997480._.js.map
